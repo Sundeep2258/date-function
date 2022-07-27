146307dfe2109749fea0a310a0fe1ea671d90226
sandeep.B <102549011+Sundeep2258@users.noreply.github.com>
2022-07-27 10:21:15 +0530

    dateobject

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ let date1 = new Date();
 console.log(date1.getFullYear()); // 1947
 console.log(date1.getMonth()); // 7
 console.log(date1.getDate());
+
+let date2 = new Date(2012, 6, 27);
+date2.setYear(2022);
+date2.setDate(28);
+
+console.log(date2);
+console.log(date1);
